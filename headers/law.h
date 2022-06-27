7955565e9319deb6d0a527a677d3ae624c75e722
Thomas Roiseux <thomas.roiseux@outlook.com>
2022-06-27 22:13:23 +0200

    Wrote comments for Law class

--- headers/law.h
+++ headers/law.h
@@ -12,6 +12,8 @@
 #ifndef LAW_H
 #define LAW_H
 
+#include <cmath>
+
 /**
  * @brief The class Law provides a generic class that will hold all the sub-classes relative to specific laws.
  * 
@@ -19,11 +21,55 @@
 class Law
 {
 public:
-    virtual double proba(double x)=0;
-    virtual double proba(long long int x)=0;
-    virtual double mean()=0;
-    virtual double stdDev()=0;
+    /**
+     * @brief Destroys the Law object.
+     * 
+     */
     virtual ~Law();
+    /**
+     * @brief Computes the probability.
+     * 
+     * @param x Value.
+     * @return double Probability.
+     */
+    virtual double proba(double x)=0;
+
+    /**
+     * @brief Computes the probability.
+     * 
+     * @param x Value.
+     * @return double Probability.
+     */
+    virtual double proba(long long int x)=0;
+
+    /**
+     * @brief Computes the expectancy.
+     * 
+     * @return double Expectancy.
+     */
+    virtual double expectancy()=0;
+
+    /**
+     * @brief Computes the expectancy.
+     * 
+     * @return double Variance.
+     */
+    virtual double variance()=0;
+
+    /**
+     * @brief Computes the m-moment.
+     * 
+     * @param m Moment.
+     * @return double Value of the moment.
+     */
+    virtual double moment(long long unsigned int m = 1) = 0;
+
+    /**
+     * @brief Computes the standard deviation.
+     * 
+     * @return double Standard deviation.
+     */
+    virtual double stdDev() { return std::sqrt(this->variance()); };
 };
 
 #endif //LAW_H
