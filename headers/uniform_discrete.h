e81af1b3474589a81ca89abea027ffff2ed96061
Thomas Roiseux <thomas.roiseux@outlook.com>
2022-06-28 12:11:22 +0200

    Added uniform discrete class

---
+++ headers/uniform_discrete.h
@@ -0,0 +1,102 @@
+/**
+ * @file uniform_discrete.h
+ * @author AiglonDore
+ * @brief Uniform discrete law.
+ * @version 0.1
+ * @date 2022-06-28
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#ifndef UNIFORMD_H
+#define UNIFORMD_H
+
+#include "discretelaw.h"
+
+/**
+ * @brief This class provides tools for uniform discrete laws.
+ * 
+ */
+class UniformDiscreteLaw : public DiscreteLaw
+{
+private:
+    long long int lowerBound;
+    long long int upperBound;
+    long long int n;
+public:
+    /**
+     * @brief Constructs a new Uniform Discrete Law object, between 0 and 1.
+     * 
+     */
+    UniformDiscreteLaw();
+
+    /**
+     * @brief Constructs a new Uniform Discrete Law object.
+     * 
+     * @param upperBound Value for upperbound.
+     * @throws 1 if upperBound is negative or null.
+     */
+    UniformDiscreteLaw(long long int upperBound);
+    
+    /**
+     * @brief Constructs a new Uniform Discrete Law object.
+     * 
+     * @param lowerBound Value for lower bound.
+     * @param upperBound Value for upperbound.
+     * @throws 1 if upperBound is lower than lowerBound.
+     */
+    UniformDiscreteLaw(long long int lowerBound, long long int upperBound);
+
+    /**
+     * @brief Destroys the Uniform Discrete Law object.
+     * 
+     */
+    virtual ~UniformDiscreteLaw();
+
+    /**
+     * @brief Get the Lower Bound object.
+     * 
+     * @return int Lower bound.
+     */
+    long long int getLowerBound() const { return lowerBound; };
+
+    /**
+     * @brief Get the Upper Bound object.
+     * 
+     * @return int Upper bound.
+     */
+    long long int getUpperBound() const { return upperBound; };
+
+    /**
+     * @brief Gets the number of elements in the support.
+     * 
+     * @return int n.
+     */
+    long long int getN() const { return n; };
+
+    /**
+     * @brief Overrides {@link DiscreteLaw} function.
+     * 
+     * @param X Value.
+     * @return double Probability.
+     * @throws 0 if X is out of bounds.
+     */
+    virtual double proba(long long int X) override;
+
+    /**
+     * @brief Overrides {@link DiscreteLaw} function.
+     * @return double Expectancy.
+     */
+    virtual double expectancy() override;
+
+    /**
+     * @brief Overrides {@link DiscreteLaw} function.
+     * 
+     * @return double Variance.
+     */
+    virtual double variance() override;
+};
+
+
+#endif //UNIFORMD_H
---
+++ src/uniform_discrete.cpp
@@ -0,0 +1,58 @@
+/**
+ * @file uniform_discrete.cpp
+ * @author AiglonDore
+ * @brief Implements {@link uniform_discrete.h}.
+ * @version 0.1
+ * @date 2022-06-28
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include "../headers/uniform_discrete.h"
+
+UniformDiscreteLaw::UniformDiscreteLaw() : lowerBound(0), upperBound(1), n(0)
+{
+    n = upperBound - lowerBound + 1;
+}
+
+UniformDiscreteLaw::UniformDiscreteLaw(long long int upperBound) : lowerBound(0), upperBound(upperBound), n(0)
+{
+    n = upperBound - lowerBound + 1;
+    if (n <= 0)
+    {
+        throw 1;
+    }
+}
+
+UniformDiscreteLaw::UniformDiscreteLaw(long long int lowerBound, long long int upperBound) : lowerBound(lowerBound), upperBound(upperBound), n(0)
+{
+    n = upperBound - lowerBound + 1;
+    if (n <= 0)
+    {
+        throw 1;
+    }
+}
+
+UniformDiscreteLaw::~UniformDiscreteLaw()
+{
+}
+
+double UniformDiscreteLaw::proba(long long int X)
+{
+    if (X < lowerBound || X > upperBound)
+    {
+        return 0;
+    }
+    return 1. / n;
+}
+
+double UniformDiscreteLaw::expectancy()
+{
+    return (upperBound + lowerBound) / 2.;
+}
+
+double UniformDiscreteLaw::variance()
+{
+    return (n * n - 1.) / 12;
+}
