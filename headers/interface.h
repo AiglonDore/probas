d7c105e5397f8a169e8b27a26195ef5e0758e561
Thomas Roiseux <thomas.roiseux@outlook.com>
2022-06-28 15:27:11 +0200

    Base for interface

---
+++ headers/interface.h
@@ -0,0 +1,19 @@
+/**
+ * @file interface.h
+ * @author AiglonDore
+ * @brief Provides functions to ask for values.
+ * @version 0.1
+ * @date 2022-06-28
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#ifndef INTERFACE_H
+#define INTERFACE_H
+
+#include <string>
+
+void execute(const std::string& command);
+
+#endif //INTERFACE_H
---
+++ src/interface.cpp
@@ -0,0 +1,15 @@
+/**
+ * @file interface.cpp
+ * @author AiglonDore
+ * @brief Implements {@link interface.h}.
+ * @version 0.1
+ * @date 2022-06-28
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include <iostream>
+#include <string>
+
+using namespace std;
