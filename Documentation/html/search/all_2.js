872ec71002e5f75b02323935e814269bb46f4525
Thomas Roiseux <thomas.roiseux@outlook.com>
2022-07-04 19:40:56 +0200

    Updated documentation

--- Documentation/html/search/all_2.js
+++ Documentation/html/search/all_2.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['execute_9',['execute',['../interface_8h.html#ab5568791303112650f21e185f132497b',1,'execute(int argc, char *argv[]):&#160;interface.cpp'],['../interface_8cpp.html#ab5568791303112650f21e185f132497b',1,'execute(int argc, char *argv[]):&#160;interface.cpp']]],
+  ['execute_9',['execute',['../interface_8h.html#a560b645bb12c5ba28b9fb2b595ac6772',1,'execute(int argc, char *argv[], bool loop=false):&#160;interface.cpp'],['../interface_8cpp.html#af56baab4bc716fd890a2fedf5c8dad41',1,'execute(int argc, char *argv[], bool loop):&#160;interface.cpp']]],
   ['expectancy_10',['expectancy',['../class_bernoulli_law.html#addc7865f6589617696a12b5eff698fc3',1,'BernoulliLaw::expectancy()'],['../class_binomial_law.html#ac51befa1963a9714dc2648589a551dfc',1,'BinomialLaw::expectancy()'],['../class_discrete_law.html#a74ae4f6ac30f60ad2aa13188e6d49b10',1,'DiscreteLaw::expectancy()'],['../class_law.html#a3e002f2837184a3145166e26944d6aea',1,'Law::expectancy()'],['../class_uniform_discrete_law.html#a9f5dc9c49ee7320c87506c7ef64468e1',1,'UniformDiscreteLaw::expectancy()']]]
 ];
