29303d66a213f6cd4df4b40954b62bab4673ba22
Thomas Roiseux <thomas.roiseux@outlook.com>
2022-07-13 11:31:54 +0200

    Fixed other null product

--- src/utils.cpp
+++ src/utils.cpp
@@ -107,7 +107,7 @@ double Utils::Operators::integral(const std::function<double(double)>& f, double
 
 double Utils::Operators::integral(std::function<double(double)>&& f, double a, bool positiveInfinite)
 {
-    double b(a * pow(10,10)),deltaI,I;
+    double b((a + 1) * pow(10,10)),deltaI,I;
     if (positiveInfinite)//Integral between a and +infinity
     {
         do
