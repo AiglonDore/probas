191812f2e56b104a88a67571b82c61c2b4607a11
Thomas Roiseux <thomas.roiseux@outlook.com>
2022-07-14 09:55:31 +0200

    Updated basis of exception class

--- headers/exn.h
+++ headers/exn.h
@@ -30,10 +30,37 @@ private:
     ExceptionType type;
     std::string message;
 public:
+    /**
+     * @brief Construct a new Exception object. Its type will be Calculus and its message will be empty.
+     * 
+     */
     Exception();
-    Exception(ExceptionType type, const std::string& message);
-    Exception(ExceptionType type, std::string&& message);
+    /**
+     * @brief Construct a new Exception object.
+     * 
+     * @param type Type of the exception.
+     * @param message Message to be displayed.
+     */
+    Exception(ExceptionType type, const std::string& message = "");
+    /**
+     * @brief Destroy the Exception object.
+     * 
+     */
     virtual ~Exception();
+    /**
+     * @brief Get the Message object.
+     * 
+     * @return const std::string& Message as a reference.
+     */
+    const std::string& getMessage() const { return message; };
+
+    void setMessage(const std::string& message) { this->message = message; };
+    /**
+     * @brief Get the Type object.
+     * 
+     * @return ExceptionType Type of the exception.
+     */
+    const ExceptionType getType() const { return type; };
 };
 
 
