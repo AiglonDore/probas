e3a0c600cd760641ceba20ecce34a628f4846b21
Thomas Roiseux <thomas.roiseux@outlook.com>
2022-07-28 10:32:48 +0200

    Wrote shell script to run container

--- probas.sh
+++ probas.sh
@@ -2,7 +2,9 @@
 #!/bin/bash
 for $str in $@; do
     if [ $str = "--docker" -o $str = "-d" ]; then
-        #docker run -d probas:latest --name probas_tools -it
+        docker run -d probas:latest --name probas_tools -it
+        docker exec -it probas_tools /bin/bash
+        docker stop probas_tools
         exit 0
     fi
 done;
