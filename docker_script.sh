615fbe44f56944c0783fa073a7222c9b319a8e37
Thomas Roiseux <thomas.roiseux@outlook.com>
2022-07-28 10:36:57 +0200

    Wrote docker script

--- docker_script.sh
+++ docker_script.sh
@@ -1,4 +1,4 @@
 #This file is licenced under the GNU General Public License v3.0
 #!/bin/bash
 make probas.out RELEASE=TRUE container_watcher.out
-#./bin/container_watcher.out
+./bin/container_watcher.out
