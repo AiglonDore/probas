da2921db61575d8f58bf6389e77ce3bb05f1abe8
Thomas Roiseux <thomas.roiseux@outlook.com>
2022-07-28 10:43:25 +0200

    Updated Powershell script for docker

--- probas.ps1
+++ probas.ps1
@@ -1,2 +1,12 @@
 # This file is licenced under the GNU General Public License v3.0
+foreach ($item in %*)
+{
+    if ($($item -eq '--docker' -or $item -eq '-d'))
+    {
+        docker run -d probas:latest --name probas_tools -it
+        docker exec -it probas_tools /bin/bash
+        docker stop probas_tools
+        exit 0
+    }
+}
 .\bin\probas.exe %*
